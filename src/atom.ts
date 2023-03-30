213d84c2fa005b799a70e23acc089b062836b4cd
Constantin Titarenko <constantin.titarenko@gmail.com>
2023-03-30 09:41:23 +0300

    discover non-universal atom structure: fullbox, variable length entry containers

--- src/atom.ts
+++ src/atom.ts
@@ -1,8 +1,21 @@
 import { File } from "./file";
 
+export enum StructureGuess {
+  Fullbox,
+  EntryCount16Bit,
+  EntryCount32Bit,
+}
+
 export interface Atom {
+  offset: number;
+
+  size: number;
   type: string;
+
+  guess?: StructureGuess;
+
   file: File;
+
   unparsed?: true;
   atoms?: Atom[];
   content?: any;
